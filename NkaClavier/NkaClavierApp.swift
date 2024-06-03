97d983e36f8932fb400c81468bd4c7dfd9caaf6f
afrcvn <afrcvn@gmail.com>
2024-06-03 16:03:22 -0400

    Initial Commit

---
+++ NkaClavier/NkaClavierApp.swift
@@ -0,0 +1,17 @@
+//
+//  NkaClavierApp.swift
+//  NkaClavier
+//
+//  Created by Samba Diawara on 2024-06-03.
+//
+
+import SwiftUI
+
+@main
+struct NkaClavierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
